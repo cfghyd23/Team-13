b5e5fea87c2991a24964cd10d92fd62245166dd8
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2023-06-11 03:07:22 +0530

    Create createReq.js

---
+++ front-end/client/src/pages/createReq.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar, Form } from components
+
+
+export default function home() {
+  return (
+    < div className="App" style={{ backgroundColor: '#D0F5BE', height:1000 }}>
+      <Navbar/>
+      <Form></Form>
+    </div>
+  )
+}
+
+
