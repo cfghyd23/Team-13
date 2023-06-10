6b00dbb32b90887d11d586241e52e8ebf7986688
Veldojsaiteja <veldojsaiteja@gmail.com>
2023-06-11 02:04:00 +0530

    modified css

--- front-end/client/src/components/Register.js
+++ front-end/client/src/components/Register.js
@@ -36,7 +36,7 @@ const Register = () => {
   };
 
   return (
-    <section className="vh-100" style={{ backgroundColor: "#508bfc" }}>
+    <section className="vh-100 sample">
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
