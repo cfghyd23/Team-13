24039d6deed288aae1b06f7de846241521052447
Dhaval Maniar <f20200305@hyderabad.bits-pilani.ac.in>
2023-06-10 19:52:02 +0530

    bridge model added

--- back-end/models/bridgeModel.js
+++ back-end/models/bridgeModel.js
@@ -1,11 +1,19 @@
 import mongoose, { mongo } from "mongoose";
 const bridgeSchema = new mongoose.Schema({
-  patient: [
+  patient: {
+    type: mongoose.ObjectId,
+    ref: "users",
+  },
+  donors: [
     {
       type: mongoose.ObjectId,
       ref: "users",
     },
   ],
+  location: {
+    type: mongoose.ObjectId,
+    ref: "users",
+  },
 });
 
 export default mongoose.model("bloodbridge", bridgeSchema);
