6b213694d413618b791ad5a097abeffa7f64af82
Veldojsaiteja <veldojsaiteja@gmail.com>
2023-06-10 18:16:02 +0530

    added configuration model

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
+const MONGO_URL =
+  "mongodb+srv://team13:bloodwarrior@blood-warrior.zgscw5d.mongodb.net/users";
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(MONGO_URL);
     console.log(`Connected to ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error in MongoDB ${error}`);
