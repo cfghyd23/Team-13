bd226fdc1899d4b045c15c803539604945b31117
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2023-06-11 02:17:28 +0530

    bridge

---
+++ front-end/client/src/pages/bridge.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import { Navbar, Card, Slider, ReqDonor } from components
+
+
+export default function home() {
+  return (
+    < div className="App" style={{ backgroundColor: '#D0F5BE', height:1000 }}>
+      <Navbar/>
+      <Card ></Card>
+      <Slider/>
+      <ReqDonor/>
+    </div>
+  )
+}
+
+
