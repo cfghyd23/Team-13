2ce4b87952ff317180e34017739759c82bb8fa9e
Veldojsaiteja <veldojsaiteja@gmail.com>
2023-06-11 06:01:53 +0530

    modified merge conflict App.js

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -3,14 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import HomePage from "./components/Homepage";
-<<<<<<< HEAD
 import home from "./pages/bridge.js";
 import Admin from "./components/Admin";
 import "./App.css";
-=======
 
-
->>>>>>> b08164745ebcf73a6ee25997b5aa40f79415b902
 function App() {
   return (
     <>
