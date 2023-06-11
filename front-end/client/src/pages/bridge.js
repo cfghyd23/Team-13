2dd35f9294989405588cd6f7275a8782fb4093fa
Veldojsaiteja <veldojsaiteja@gmail.com>
2023-06-11 06:37:27 +0530

    modified bridge.js

--- front-end/client/src/pages/bridge.js
+++ front-end/client/src/pages/bridge.js
@@ -1,20 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Card from "./components/Card";
-import Navbar from "./components/Navbar";
-import ReqDonor from "./components/ReqDonor";
-import Slider from "./components/Slider";
-
+import Card from "../components/Card.js";
+import Navbar from "../components/Navbar.js";
+import ReqDonor from "../components/ReqDonor.js";
+import Slider from "../components/Slider.js";
 
 export default function home() {
-  
   return (
-    < div className="App" style={{ backgroundColor: '#D0F5BE', height:1000 }}>
-      <Navbar/>
-      <Card ></Card>
-      <Slider/>
-      <ReqDonor/>
+    <div className="App" style={{ backgroundColor: "#D0F5BE", height: 1000 }}>
+      <Navbar />
+      <Card></Card>
+      <Slider />
+      <ReqDonor />
     </div>
-  )
+  );
 }
-
-
