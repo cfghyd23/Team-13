23c5a64b065c5d4b4be833051ced40d1180cea00
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2023-06-11 08:54:21 +0530

    colour

--- front-end/client/src/pages/createReq.js
+++ front-end/client/src/pages/createReq.js
@@ -4,7 +4,7 @@ import { Navbar, Form } from components
 
 export default function home() {
   return (
-    < div className="App" style={{ backgroundColor: '#D0F5BE', height:1000 }}>
+    < div className="App" style={{ backgroundColor: '#F6F1F1', height:1000 }}>
       <Navbar/>
       <Form></Form>
     </div>
