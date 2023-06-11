0de4dbf8ada49d8988771e8d19afc9e7e6c8738b
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2023-06-11 08:57:34 +0530

    deets

---
+++ front-end/client/src/components/card.js
@@ -0,0 +1,115 @@
+import React from "react";
+import "./App.css";
+function Card(){
+  return (
+   
+      
+   
+    <div className='display'>
+    <div className="row">
+    <div className='column grid-item'>
+    
+        <card>
+    <p>Name:<span><img src='https://www.nicepng.com/png/detail/364-3647802_blood-symbol-png-blood-donation-app-logo.png'/></span></p>
+    <p>Email:</p>
+    <p>Location:</p>
+    <p>Phone:</p>
+    <p>Blood Available:</p>
+    </card>
+  <hr/>
+    <table style={{width:180}}>
+  <tr>
+    <th>Type</th>
+    <th >Quantity</th>
+  </tr>
+</table>
+   </div>
+   <div className="column grid-item" >
+        <card>
+    <p>Name:<span><img src='https://www.nicepng.com/png/detail/364-3647802_blood-symbol-png-blood-donation-app-logo.png'/></span></p>
+    <p>Email:</p>
+    <p>Location:</p>
+    <p>Phone:</p>
+    <p>Blood Available:</p>
+    </card>
+  <hr/>
+    <table style={{width:180}}>
+  <tr>
+    <th>Type</th>
+    <th >Quantity</th>
+  </tr>
+</table>
+   </div>
+   <div className="column grid-item" >
+        <card>
+    <p>Name:<span><img src='https://www.nicepng.com/png/detail/364-3647802_blood-symbol-png-blood-donation-app-logo.png'/></span></p>
+    <p>Email:</p>
+    <p>Location:</p>
+    <p>Phone:</p>
+    <p>Blood Available:</p>
+    </card>
+  <hr/>
+    <table style={{width:180}}>
+  <tr>
+    <th>Type</th>
+    <th >Quantity</th>
+  </tr>
+</table>
+   </div>
+   <div className="column grid-item" >
+        <card>
+    <p>Name:<span><img src='https://www.nicepng.com/png/detail/364-3647802_blood-symbol-png-blood-donation-app-logo.png'/></span></p>
+    <p>Email:</p>
+    <p>Location:</p>
+    <p>Phone:</p>
+    <p>Blood Available:</p>
+    </card>
+  <hr/>
+    <table style={{width:180}}>
+  <tr>
+    <th>Type</th>
+    <th >Quantity</th>
+  </tr>
+</table>
+   </div>
+   <div className="column grid-item" >
+        <card>
+    <p>Name:<span><img src='https://www.nicepng.com/png/detail/364-3647802_blood-symbol-png-blood-donation-app-logo.png'/></span></p>
+    <p>Email:</p>
+    <p>Location:</p>
+    <p>Phone:</p>
+    <p>Blood Available:</p>
+    </card>
+  <hr/>
+    <table style={{width:180}}>
+  <tr>
+    <th>Type</th>
+    <th >Quantity</th>
+  </tr>
+</table>
+   </div>
+   <div className="column grid-item" >
+        <card>
+    <p>Name:<span><img src='https://www.nicepng.com/png/detail/364-3647802_blood-symbol-png-blood-donation-app-logo.png'/></span></p>
+    <p>Email:</p>
+    <p>Location:</p>
+    <p>Phone:</p>
+    <p>Blood Available:</p>
+    </card>
+  <hr/>
+    <table style={{width:180}}>
+  <tr>
+    <th>Type</th>
+    <th >Quantity</th>
+  </tr>
+</table>
+   </div>
+  
+
+  </div>
+  </div>
+  )
+}
+
+
+export default Card
