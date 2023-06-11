70dbcf9b6ce1b9495cb87fa84141ec5e042c1c79
Dhaval Maniar <f20200305@hyderabad.bits-pilani.ac.in>
2023-06-11 06:37:36 +0530

    part 1

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -5,10 +5,9 @@ import Register from "./components/Register";
 import HomePage from "./components/Homepage";
 <<<<<<< HEAD
 import home from "./pages/bridge.js";
+=======
 import Admin from "./components/Admin";
 import "./App.css";
-=======
-
 
 >>>>>>> b08164745ebcf73a6ee25997b5aa40f79415b902
 function App() {
--- front-end/client/src/pages/bridge.js
+++ front-end/client/src/pages/bridge.js
@@ -4,17 +4,13 @@ import Navbar from "./components/Navbar";
 import ReqDonor from "./components/ReqDonor";
 import Slider from "./components/Slider";
 
-
 export default function home() {
-  
   return (
-    < div className="App" style={{ backgroundColor: '#D0F5BE', height:1000 }}>
-      <Navbar/>
-      <Card ></Card>
-      <Slider/>
-      <ReqDonor/>
+    <div className="App" style={{ backgroundColor: "#D0F5BE", height: 1000 }}>
+      <Navbar />
+      <Card></Card>
+      <Slider />
+      <ReqDonor />
     </div>
-  )
+  );
 }
-
-
